66ca5b5bc9f33a3900f9ea44459b6de1cf6faec2
BHARATH-E-007 <bharathpm007@gmail.com>
2025-04-19 11:58:52 +0530

    Chore:ReturnTheColumnsIndex

---
+++ ConvertAUpperCaseAlphabetic.c
@@ -0,0 +1,17 @@
+boundedArray getColumn(int R, int C, int matrix[] [C], int K)
+
+{
+
+struct BoundedArray result (struct BoundedArray")malloc(sizeof(struct
+
+BoundedArray)); result->arr=(int*)malloc(sizeof(int)"R);
+
+result->SIZE-R;
+
+for (int i=0;i<n;i++){
+
+}
+
+result->arr[i]=matrix[k-1][i];
+
+return result;
